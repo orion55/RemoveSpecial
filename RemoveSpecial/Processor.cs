02e6d084a4a1389101f7607950360370afe0bbb8
Oleg Grebenev <oleg.grebenev@rokolabs.com>
2022-04-22 10:22:37 +0500

    Final commit

--- RemoveSpecial/Processor.cs
+++ RemoveSpecial/Processor.cs
@@ -7,25 +7,40 @@ public class Processor
     private readonly string _path;
     private readonly Regex _compiledUnicodeRegex = new Regex(@"[^\u0000-\u007F|^\u0400-\u04FF]", RegexOptions.Compiled);
     private readonly Regex _multipleSpacesRegex = new Regex(@"\s+", RegexOptions.Compiled);
+
     public Processor(string path)
     {
         if (string.IsNullOrEmpty(path))
             throw new Exception("The \"path\" value is empty");
-        if (!Directory.Exists(path)) 
-            throw new Exception("The directory "+path+" does not exist!");
-        
+        if (!Directory.Exists(path))
+            throw new Exception("The directory " + path + " does not exist!");
+
         _path = path;
     }
 
     public void Rename()
     {
         var files = Directory.GetFiles(_path, "*.*", SearchOption.AllDirectories);
-        foreach (var fileName in files)
+        foreach (var file in files)
         {
-            var newName = StripCharacters(fileName);
-            Console.WriteLine("{0} ", newName);
+            var fileName = Path.GetFileName(file);
+            var dirName = Path.GetDirectoryName(file);
+
+            var newFileName = StripCharacters(fileName);
+
+            if (fileName.Equals(newFileName)) continue;
+            
+            try
+            {
+                File.Move(dirName + '\\' + fileName, dirName + '\\' + newFileName);
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("New file name => {0} ", dirName + '\\' + newFileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
-        
     }
 
     private string StripCharacters(string inputValue)
--- RemoveSpecial/Program.cs
+++ RemoveSpecial/Program.cs
@@ -6,6 +6,9 @@ try
 {
     var processor = new Processor(settings.Path);
     processor.Rename();
+    
+    Console.WriteLine("Press any key to exit the process...");
+    Console.ReadKey();
 }
 catch (Exception ex)
 {
